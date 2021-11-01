f64eabf769ae91a3c7b192f8dbde370d89e3ff22
RayWang <2271272653@qq.com>
2021-11-01 13:16:57 +0800

    update

--- Common/test.js
+++ Common/test.js
@@ -8,34 +8,79 @@
  */
 auto.waitFor();
 
+device.wakeUpIfNeeded();
+
+let {
+    height,
+    width
+} = device
+let x = width / 2
+let y1 = (height / 3) * 2
+let y2 = height / 3
+swipe(x, y1, x + 5, y2, 500)
+
 console.show();
 
-var logger = new RayHamiLog('测试');
-var t = logger.test();
-console.log(t);
+var logger = new RayHamiLog();
 
-logger.pushPlusNotify('测试标题', '测试内容');
+logger.log(1);
+logger.log(2);
+logger.log('测试');
+logger.pushAllLogs();
 
 
-//let logger=new RayHamiLog();
-//logger.sendNotify('测试标题','测试内容');
-
-function RayHamiLog(name) {
+function RayHamiLog(scriptName) {
     const {
         PUSH_PLUS_TOKEN,
-        PUSH_PLUS_USER
-    } = hamibot.env
+        PUSH_PLUS_USER,
+        SCKEY
+    } = hamibot.env;
 
-    this.name = name;
-    this.getName = function () {
-        return this.name;
+    this.scriptName = scriptName;
+    this.defaultAuthor = '\n\n by Ray';
+    this.defaultTitle = this.scriptName ? this.scriptName : '推送-' + new Date();
+
+    this.logMsgList = new Array();
+
+    this.log = function (msg) {
+        toastLog(msg);//以气泡显示信息几秒，同时也会输出到控制台
+        //console.log(msg);//发送到控制台
+        hamibot.postMessage(msg); //发送到控制台的脚本消息
+
+        this.logMsgList.push(msg);//加到缓存里，用作最后的远端推送
     }
 
-    this.test = function () {
-        return PUSH_PLUS_TOKEN;
+    this.pushAllLogs = function (title, params, author) {
+        if (!title) title = this.defaultTitle;
+        if (!params) params = {};
+        if (!author) author = this.defaultAuthor;
+
+        this.log('开始推送日志：');
+
+        if (this.logMsgList.length <= 0) return;
+
+        let msg = "";
+
+        this.logMsgList.forEach(element => {
+            msg = msg + element + '\n';
+        });
+
+        msg += author;//增加作者信息，防止被贩卖等
+
+        this.pushMsg(title, msg, params);
+
+        this.log('日志推送结束');
+    };
+
+    this.pushMsg = function (title, msg, params) {
+        //提供6种通知
+        Promise.all([
+            this.pushPlusNotify(title, msg),
+            this.serverNotify(title, msg)
+        ])
     }
 
-    this.pushPlusNotify = function (text, desp) {
+    this.pushPlusNotify = function (title, desp) {
         return new Promise(resolve => {
             if (PUSH_PLUS_TOKEN) {
                 var url = 'http://www.pushplus.plus/send';
@@ -43,7 +88,7 @@ function RayHamiLog(name) {
                 desp = desp.replace(/[\n\r]/g, '<br>'); // 默认为html, 不支持plaintext
                 const body = {
                     token: PUSH_PLUS_TOKEN,
-                    title: text,
+                    title: title,
                     content: desp,
                     topic: PUSH_PLUS_USER
                 };
@@ -67,9 +112,64 @@ function RayHamiLog(name) {
                     console.log(e, resp);
                 }
                 finally {
+                    resolve(res);
                 }
-            } else {
+            }
+            else {
+                resolve(res);
                 console.log('您未提供push+推送所需的PUSH_PLUS_TOKEN，取消push+推送消息通知🚫\n');
+            }
+        })
+    };
+
+    this.serverNotify = function (title, desp, time) {
+        if (!time) time = 2100;
+        return new Promise(resolve => {
+            if (SCKEY) {
+                //微信server酱推送通知一个\n不会换行，需要两个\n才能换行，故做此替换
+                desp = desp.replace(/[\n\r]/g, '\n\n');
+                const options = {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                }
+                let url = SCKEY.includes('SCT') ?
+                    ('https://sctapi.ftqq.com/' + SCKEY + '.send')
+                    : ('https://sc.ftqq.com/' + SCKEY + '.send');
+                let body = 'text=' + title + '&desp=' + 'desp';
+                setTimeout(() => {
+                    let res;
+                    try {
+                        res = http.post(url, body, options);
+                        if (res.statusCode != 200) {
+                            console.log('发送通知调用API失败！！\n')
+                        }
+                        else {
+                            let data = res.body.json();
+                            //server酱和Server酱·Turbo版的返回json格式不太一样
+                            if (data.errno === 0 || data.data.errno === 0) {
+                                console.log('server酱发送通知消息成功🎉\n')
+                            }
+                            else if (data.errno === 1024) {
+                                // 一分钟内发送相同的内容会触发
+                                console.log('server酱发送通知消息异常: ' + data.errmsg + '\n')
+                            }
+                            else {
+                                console.log('server酱发送通知消息异常\n' + JSON.stringify(data))
+                            }
+                        }
+                    }
+                    catch (e) {
+                        console.log(e, resp);
+                    }
+                    finally {
+                        resolve(res);
+                    }
+                }, time)
+            }
+            else {
+                //console.log('\n\n您未提供server酱的SCKEY，取消微信推送消息通知🚫\n');
+                resolve()
             }
         })
     }
